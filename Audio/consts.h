c6a99ccba95fa87557cc08e0814fb2ea1a73186c
Mathias Balling <math260r@gmail.com>
2023-12-18 14:46:20 +0100

    Match index to string

--- Audio/consts.h
+++ Audio/consts.h
@@ -65,13 +65,19 @@ enum class Operation {
   STRING
 };
 
-const std::string indexToOperation[12] = {
-    "Error",    "Acknowledge",
-    "Forward",  "Backward",
-    "Left",     "Right",
-    "Stop",     "Update Magnetude Threshold",
-    "Movement", "Relative Coordinate",
-    "LIDAR",    "String"};
+const std::string indexToOperation[13] = {"Error",
+                                          "Acknowledge",
+                                          "Forward",
+                                          "Backward",
+                                          "Left",
+                                          "Right",
+                                          "Stop",
+                                          "Update Fade Steps",
+                                          "Update Magnetude Threshold",
+                                          "Movement",
+                                          "LIDAR",
+                                          "Relative Coordinate",
+                                          "String"};
 
 const char indexToDtmf[16] = {'1', '2', '3', 'A', '4', '5', '6', 'B',
                               '7', '8', '9', 'C', '*', '0', '#', 'D'};
